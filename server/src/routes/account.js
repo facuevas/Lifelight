74244d9d3829d23262b3884f77f212ed683fcb16
facuevas <franciscuevas4@gmail.com>
2020-07-03 11:22:08 -0700

    working on unfollowing

--- server/src/routes/account.js
+++ server/src/routes/account.js
@@ -86,7 +86,8 @@ router.get('/followers', passport.authenticate('jwt', {session: false}), (req, r
 })
 
 // Adds a another user to the user's following list
-router.put('/:username/follow', passport.authenticate('jwt', {session: false}), (req, res) => {
+// TODO -> CURRENTLY STORES INFORMATION
+router.post('/:username/follow', passport.authenticate('jwt', {session: false}), (req, res) => {
     Account.findOne({username: req.params.username}).populate('_id').exec((err, document) => {
         if (err) {
             res.status(500).json({message: {msgBody: "Error has occured. Try again", msgError: true}});
@@ -104,5 +105,27 @@ router.put('/:username/follow', passport.authenticate('jwt', {session: false}), 
         }
     })
 });
+
+
+// TODO -> Currently deletes all people in followings list
+router.post('/:username/unfollow', passport.authenticate('jwt', {session: false}), (req, res) => {
+    Account.findOne({username: req.params.username}).populate('_id').exec((err, document) => {
+        if (err) {
+            res.status(500).json({message: {msgBody: "Error has occured. Try again", msgError: true}});
+        }
+        else {
+            //console.log(document._id._id);
+            req.user.following = req.user.following.filter(id => id !== document._id._id);
+            req.user.save(err => {
+                if (err) {
+                    res.status(400).json({message: {msgBody: "Error has occured unfollowing", msgError: true}});
+                }
+                else {
+                    res.status(200).json({message: {msgBody: "You have stopped following this user", msgError: false}});
+                }
+            })
+        }
+    })
+})
 
 module.exports = router;
