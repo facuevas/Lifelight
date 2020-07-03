4e6fee272b7a39cd14a52caf702122c7a071bcf5
facuevas <franciscuevas4@gmail.com>
2020-07-03 11:41:02 -0700

    finished following. TODO: unfollowing

--- server/src/routes/account.js
+++ server/src/routes/account.js
@@ -86,30 +86,29 @@ router.get('/followers', passport.authenticate('jwt', {session: false}), (req, r
 })
 
 // Adds a another user to the user's following list
-// TODO -> SET USER AS A FOLLOWER TO OTHER ACCOUNT
+// and conversely adds the user to the other user's followers list
 router.post('/:username/follow', passport.authenticate('jwt', {session: false}), (req, res) => {
-    Account.findOne({username: req.params.username}).populate('_id').exec((err, document) => {
-        if (err) {
-            res.status(500).json({message: {msgBody: "Error has occured. Try again", msgError: true}});
-        }
-        else {
-            req.user.following.push(document._id);
-            console.log(document.followers);
-            req.user.save(err => {
-                if (err) {
-                    res.status(500).json({message: {msgBody: "Error has occured. Try again", msgError: true}});
-                }
-                else {
-                    document.followers.push(req.user._id);
-                    document.save(err => {
+    Account.findOne({username: req.params.username}, (err, acc) => {
+        acc.followers.push(req.user._id);
+        let followedUser = acc._id;
+        acc.save((err) => {
+            if (err) {
+                res.status(500).json({message: {msgBody: "Error has occured. Try again", msgError: true}});
+            }
+            else {
+                Account.findOne({username: req.user.username}, (err, acc) => {
+                    acc.following.push(followedUser);
+                    acc.save((err) => {
                         if (err) {
-                            res.status(500).json({message: {msgBody: "Error updating follower list on other user", msgError: true}});
+                            res.status(500).json({message: {msgBody: "Error has occured. Try again", msgError: true}});
+                        }
+                        else {
+                            res.status(500).json({message: {msgBody: "Success", msgError: false}});
                         }
                     })
-                    res.status(200).json({message: {msgBody: "Successfully followed!", msgError: false}});
-                }
-            })
-        }
+                })
+            }
+        })
     })
 });
 
@@ -122,7 +121,7 @@ router.post('/:username/unfollow', passport.authenticate('jwt', {session: false}
         }
         else {
             //console.log(document._id._id);
-            req.user.following = req.user.following.filter(id => id !== document._id._id);
+            req.user.following = req.user.following.filter(id => id === document._id._id);
             req.user.save(err => {
                 if (err) {
                     res.status(400).json({message: {msgBody: "Error has occured unfollowing", msgError: true}});
