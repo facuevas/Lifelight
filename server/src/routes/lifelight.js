25ed8a5e31c4a57eb7400b819a49d553e0a3f344
facuevas <franciscuevas4@gmail.com>
2020-07-08 12:37:53 -0700

    fixed bugs on GET /:username/:lifelight_id route and finished DELETE /:username/:lifelight_id route

--- server/src/routes/lifelight.js
+++ server/src/routes/lifelight.js
@@ -36,29 +36,67 @@ router.get('/lifelights', passport.authenticate('jwt', {session: false}), (req, 
     });
 });
 
+router.get('/feed', passport.authenticate('jwt', {session: false}), (req, res) => {
+    Account.findById({_id: req.user._id}).populate('following').exec((err, document) => {
+        if (err) {
+            res.status(500).json({message: {msgBody: "Error has occured displaying following list.", msgError: true}});
+        }
+        else {
+            res.status(200).json({following: document.following, authenticated: true});
+        }
+    });
+});
+
 router.get('/:username', passport.authenticate('jwt', {session: false}), (req, res) => {
     Account.findOne({username: req.params.username}).populate('lifelights').exec((err, document) => {
         if (err) {
             res.status(500).json({message: {msgBody: "Error has occured. Try again", msgError: true}});
         }
         else {
-            res.status(200).json({lifelights: document.lifelights, authenicated: true})
+            if (!document) {
+                res.status(404).json({message: {msgBody: "User not found", msgError: true}});
+            }
+            else {
+                res.status(200).json({lifelights: document.lifelights, authenicated: true});
+            }
         }
     });
 });
 
+
 router.get('/:username/:lifelight_id', passport.authenticate('jwt', {session: false}), (req, res) => {
-    Lifelight.findById({_id: req.params.lifelight_id}, (err, ll) => {
+    // Query the account
+    let lifelight = req.params.lifelight_id;
+    Account.findOne({username: req.params.username}, (err, acc) => {
+        // If there is an error, throw one
         if (err) {
-            res.status(500).json({message: {msgBody: "Lifelight could not be found", msgError: true}});
+            res.status(500).json({message: {msgBody: "Error has occured", msgError: true}});
         }
         else {
-            res.status(200).json({username: req.params.username, lifelight: ll})
+            // Check if the account exists
+            if (!acc) {
+                res.status(404).json({message: {msgBody: "User not found", msgError: true}});
+            }
+            else {
+                // Check if the lifelight belongs to the user
+                if (acc.lifelights.includes(lifelight)) {
+                    Lifelight.findById({_id: lifelight}, (err, document) => {
+                        if (err) {
+                            res.status(500).json({message: {msgBody: "Error retrieving lifelight", msgError: true}});
+                        }
+                        else {
+                            res.status(200).json({username: req.params.username, lifelight: document});
+                        }
+                    });
+                }
+                else {
+                    res.status(404).json({message: {msgBody: "Lifelight not found", msgError: true}});
+                }
+            }
         }
     })
-})
+});
 
-// TODO. DOES NOT CURRENTLY WORK
 router.delete('/:username/:lifelight_id', passport.authenticate('jwt', {session: false}), (req, res) => {
     if (req.params.username !== req.user.username) {
         res.status(401).json({message: {msgBody: "You can only delete your own lifelights.", msgError: true}});
